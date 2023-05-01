8cab66a0e3eea83a4aab4dc32f1d97ec07314643
Anne Staff <anne.staff@pm.me>
2023-05-01 13:01:16 +0200

    first commit

---
+++ fitdistrmachin.r
@@ -0,0 +1,39 @@
+gpigs <- read.table("surv.gpigs.txt", header = T, sep = ";")
+gpigs.noncensored <- gpigs[gpigs$censored == 0,]
+
+gpigs.mc <- gpigs.noncensored$lifetime[gpigs.noncensored$regime == "M_C"]
+gpigs.mc.with_censored <- gpigs$lifetime[gpigs$regime == "M_C"]
+gpigs.m43 <- gpigs.noncensored$lifetime[gpigs.noncensored$regime == "M_4.3"]
+
+summary(gpigs.mc)
+summary(gpigs.mc.with_censored) # on garde celui-là
+summary(gpigs.m43)
+
+hist(gpigs.noncensored$lifetime, breaks = 26)
+
+dloglogis <- function(x, beta, alpha) {
+  res <- (beta/alpha) * (x/alpha)^(beta-1)*(1+(x/alpha)^beta)^-2
+  return(res)
+}
+
+ploglogis <- function(q, alpha, beta) {
+  res <- 1 / (1 + (q/alpha)^beta)
+  return(res)
+}
+
+qloglogis <- function(p, alpha, beta) {
+  res <- alpha * ((1/p) - 1)^(1/beta)
+  return(res)
+}
+
+
+library(fitdistrplus)
+# MC regime
+fw <- fitdist(gpigs.mc, "loglogis", method = "mle", start=list(alpha=10, beta=5))
+fw$estimate
+denscomp(list(fw), main = "MC regime")
+
+# M4.3 regime
+fw <- fitdist(gpigs.noncensored$lifetime[gpigs.noncensored$regime == "M_4.3"], "loglogis", method = "mle", start=list(alpha=10, beta=5))
+fw$estimate
+denscomp(list(fw), main = "M4.3 regime")
---
+++ newton-raphson.r
@@ -0,0 +1,7 @@
+### NEWTON RAPHSON ALGORITHM ###
+# Goal: find the root (x axis intersection) of a function, works for real and complex functions
+# 1. pick a random point x of the function ("first guess")
+# 2. find the tangent to the curve at that point x
+# 3. find the intersection of the tangent with the x axis and make it your new x
+# 4. repeat until you're close enough (delta-y below a certain threshold which defines that you're on a point)
+
