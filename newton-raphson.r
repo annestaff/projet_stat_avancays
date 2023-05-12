a8009eb0cd2a18204240b034746fd7883d5554f5
Anne Staff <anne.staff@pm.me>
2023-05-12 15:07:51 +0200

    newton raphson wooooorks 🥳🥳🥳🥳

--- newton-raphson.r
+++ newton-raphson.r
@@ -116,3 +116,69 @@ gpigs.m43 <- gpigs.noncensored$lifetime[gpigs.noncensored$regime == "M_4.3"]
 NR_fit_LogLogistic(gpigs.m43, c(150, 3))
 # pour comparaison le résultat de fitdist:  alpha = 152.389696   beta = 3.012415 
 # on est bien loin
+
+# next try avec la matrice hessienne
+loglogistic_hessian <- function(x, n, alpha, beta) {
+  hessian <- matrix(0, nrow = 2, ncol = 2)
+  for (i in 1:length(x)) {
+    xi <- x[i]
+    hessian[1, 1] <- hessian[1, 1] + (beta^2 / xi^2) * ((xi / alpha)^(beta - 1)) / ((1 + (xi / alpha)^beta)^2)
+    hessian[1, 2] <- hessian[1, 2] + (2 * xi^beta * log(xi / alpha)) / ((1 + (xi / alpha)^beta)^2)
+    hessian[2, 1] <- hessian[2, 1] + (2 * xi^beta * log(xi / alpha)) / ((1 + (xi / alpha)^beta)^2)
+    hessian[2, 2] <- hessian[2, 2] - (xi^beta * log(xi / alpha)) / ((1 + (xi / alpha)^beta)^2)
+  }
+  hessian[1, 1] <- (n * beta / alpha^2) - (2 * hessian[1, 1])
+  hessian[2, 2] <- -(n / beta^2) - (2 * hessian[2, 2])
+  hessian[2, 1] <- -(n / alpha) + (2 * hessian[2, 1])
+  hessian[1, 2] <- hessian[2, 1]
+  hessian
+}
+
+
+NR_fit_LogLogistic_hessian <- function(x, params_0, eps = 0.001)
+{
+  params <- params_0
+  n <- length(x)
+  
+  sumlogx <- sum(log(x))
+  
+  diff <- Inf
+  alpha <- params_0[1]
+  beta <- params_0[2]
+  
+  while (diff > eps)
+  {
+    params.old <- params
+    
+    # Intermediary variables
+    w1 <- sum((x/alpha)^beta * (1 + (x/alpha)^beta)^-1)
+    w2 <- sum(log(x) * (x/alpha)^beta * (1 + (x/alpha)^beta)^-1)
+    w3 <- sum(log(x)^2 * (x/alpha)^beta * (1 + (x/alpha)^beta)^-1)
+    
+    # Gradient vector
+    logx_over_alpha <- log(x/alpha)
+    if (any(is.nan(logx_over_alpha))) {
+      s <- c(0, 0)
+    } else {
+      s <- c(n/beta - sum(log(1 + (x/alpha)^beta)), 
+             -n*alpha/beta^2 +
+               sum(logx_over_alpha * (x/alpha)^beta * (1 + (x/alpha)^beta)^-2))
+    }
+    
+    # Hessian matrix
+    H <- loglogistic_hessian(x, n, alpha, beta)
+    
+    # Update parametres using jacobian matrix
+    params <- params - solve(H) %*% s
+    
+    alpha <- params[1]
+    beta <- params[2]
+    
+    diff <- abs(sum(params - params.old))
+  }
+  
+  list(params = params, H = H)
+}
+
+NR_fit_LogLogistic_hessian(gpigs.m43, c(150, 3))
+# IT WOOOOORKS WOOHOOOOO
