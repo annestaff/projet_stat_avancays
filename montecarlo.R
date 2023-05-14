d29864101dae8b62f775bd25a78cccfdcfb00d20
Anne Staff <github@mlle.io>
2023-05-14 09:54:39 +0000

    final version

--- montecarlo.R
+++ montecarlo.R
@@ -8,9 +8,9 @@ set.seed(42)
 #####################################
 # some values needed for later
 n_values <- c(100, 500, 1000, 5000, 10000) # sample sizes
-I <- 5000 # no. of iterations
-alpha0 <- 199.5
-beta0 <- 5.55
+I <- 1000 # no. of iterations
+alpha0 <- 452.6
+beta0 <- 2.2
 #####################################
 # censoring function, for a given quantile sets the max value and censors everything above
 censor <- function(x, p = 0.9) {
@@ -33,19 +33,19 @@ mc <- function(n, I, alpha0, beta0) {
     dat <- rloglogis(n, alpha0, beta0)
     # censor the top 10%
     dat_censored <- censor(dat)
-    est_temp <- NR_fit_LogLogistic_hessian(dat_censored, c(alpha0, beta0), 0.000001)$params
+    est_temp <- NR_fit_LogLogistic_hessian(dat_censored, c(alpha0, beta0), 0.0001)$params
     est <- rbind(est, c(est_temp[1,1], est_temp[2,1]))
   }
   return(c(mean((est[,1] - alpha0)^2), mean((est[,2] - beta0)^2)))
 }
-test_mc <- mc(100, I, alpha0, beta0)
+# test_mc <- mc(100, I, alpha0, beta0)
 #####################################
 res <- matrix(nrow = 2, ncol = 0)
 for (i in 1:length(n_values)) {
   res <- cbind(res, mc(n_values[i], I, alpha0, beta0))
 }
-colnames(res) <- n_values
-write.table(res, "mc_res_with_censoring_5k_iterations_199p5_5p55.csv")
+colnames(res) <- n_values[1:4]
+write.table(res, "mc_res_with_censoring_1k_iterations_452_2.csv")
 #####################################
 # res <- read.table("mc_res.csv")
 
--- newton-raphson.r
+++ newton-raphson.r
@@ -5,132 +5,30 @@
 # 3. find the intersection of the tangent with the x axis and make it your new x
 # 4. repeat until you're close enough (delta-y below a certain threshold which defines that you're on a point)
 
-## example on weibull from litterature
-library(ggplot2)
-GP_data <-
-  data.frame("lifetimes" = c(10, 33, 44, 56, 59, 72, 74, 77, 92, 93, 96, 100, 100, 102, 105, 10))
-NR_fit_Weibull <- function(x, beta_0, eps = 0.000001)
-{
-  beta <- beta_0
+score_alpha <- function(x, alpha, beta) {
   n <- length(x)
-  
-  sumlogx <- sum(log(x))
-  
-  diff <- 1
-  alpha <- beta_0[1]
-  theta <- beta_0[2]
-  
-  
-  while (diff > eps)
-  {
-    beta.old <- beta
-    
-    w1 <- sum(x ^ alpha)
-    w2 <- sum(x ^ alpha * log(x))
-    w3 <- sum(x ^ alpha * (log(x) ^ 2))
-    s <- c(n / alpha + sumlogx - w2 / theta,-n /
-            theta + w1 / theta ^ 2)
-    J <- matrix(c(
-      -n / alpha ^ 2 - w3 / theta,
-      w2 / theta ^ 2,
-      w2 / theta ^ 2,
-      n / theta ^ 2 - w1 * 2 / (theta ^ 3)
-    ),
-    ncol <- 2)
-    beta <- beta - solve(J) %*% s
-    alpha <- beta[1]
-    theta <- beta[2]
-    diff <- sum(abs(beta - beta.old))
-  }
-  list(beta = beta, J = J)
-}
-lifetimes <- GP_data$lifetimes[GP_data$lifetimes != 0]
-NR_fit_Weibull(x = lifetimes,
-               beta_0 = c(5, 155),
-               eps = 0.001)
-
-
-dloglogis <- function(x, alpha, beta) {
-  res <- (beta/alpha) * (x/alpha)^(beta-1)*(1+(x/alpha)^beta)^-2
-  return(res)
+  score <- -((n * beta) / alpha) + 2 * sum((beta / x) * ((x / alpha)^(beta - 1)) / (1 + (x / alpha)^beta))
+  return(score)
 }
 
-## for log-logistic ça donne ça
-NR_fit_LogLogistic <- function(x, params_0, eps = 0.001)
-{
-  params <- params_0
+score_beta <- function(x, alpha, beta) {
   n <- length(x)
-  
-  sumlogx <- sum(log(x))
-  
-  diff <- Inf
-  alpha <- params_0[1]
-  beta <- params_0[2]
-  
-  while (diff > eps)
-  {
-    params.old <- params
-    
-    # Gradient vector
-    logx_over_alpha <- log(x/alpha)
-    if (any(is.nan(logx_over_alpha))) {
-      s <- c(0, 0)
-    } else {
-      s <- c(n/beta - sum(log(1 + (x/alpha)^beta)), 
-             -n*alpha/beta^2 +
-               sum(logx_over_alpha * (x/alpha)^beta * (1 + (x/alpha)^beta)^-2))
-    }
-    
-    # Jacobian matrix
-    if (any(is.nan(logx_over_alpha))) {
-      J <- matrix(0, nrow=2, ncol=2)
-    } else {
-      J <- matrix(0, nrow=2, ncol=2)
-      J[1, 1] <- n/alpha^2 + beta * sum((x/alpha)^(2*beta)/(1 + (x/alpha)^beta)^2)
-      J[1, 2] <- -sum((x/alpha)^beta * logx_over_alpha/(1 + (x/alpha)^beta))
-      J[2, 1] <- -beta * sum((x/alpha)^beta * logx_over_alpha/(1 + (x/alpha)^beta))
-      J[2, 2] <- n/beta^2 - sum((log(1 + (x/alpha)^beta))^2)
-    }
-    
-    # Update parametres using jacobian matrix
-    params <- params - solve(J) %*% s
-    
-    alpha <- params[1]
-    beta <- params[2]
-    
-    diff <- abs(sum(params - params.old))
-  }
-  
-  list(params = params, J = J)
+  score <- (n / beta) - (n * log(alpha)) + sum(log(x)) + 2 * sum(((x / alpha)^beta) * log(x / alpha) / (1 + (x / alpha)^beta))
+  return(score)
 }
 
-
-gpigs <- read.table("surv.gpigs.txt", header = T, sep = ";")
-gpigs.m43 <- gpigs.noncensored$lifetime[gpigs.noncensored$regime == "M_4.3"]
-
-NR_fit_LogLogistic(gpigs.m43, c(150, 3))
-# pour comparaison le résultat de fitdist:  alpha = 152.389696   beta = 3.012415 
-# on est bien loin
-
-# next try avec la matrice hessienne
 loglogistic_hessian <- function(x, n, alpha, beta) {
   hessian <- matrix(0, nrow = 2, ncol = 2)
-  for (i in 1:length(x)) {
-    xi <- x[i]
-    hessian[1, 1] <- hessian[1, 1] + (beta^2 / xi^2) * ((xi / alpha)^(beta - 1)) / ((1 + (xi / alpha)^beta)^2)
-    hessian[1, 2] <- hessian[1, 2] + (2 * xi^beta * log(xi / alpha)) / ((1 + (xi / alpha)^beta)^2)
-    hessian[2, 1] <- hessian[2, 1] + (2 * xi^beta * log(xi / alpha)) / ((1 + (xi / alpha)^beta)^2)
-    hessian[2, 2] <- hessian[2, 2] - (xi^beta * log(xi / alpha)) / ((1 + (xi / alpha)^beta)^2)
-  }
-  hessian[1, 1] <- (n * beta / alpha^2) - (2 * hessian[1, 1])
-  hessian[2, 2] <- -(n / beta^2) - (2 * hessian[2, 2])
-  hessian[2, 1] <- -(n / alpha) + (2 * hessian[2, 1])
-  hessian[1, 2] <- hessian[2, 1]
-  hessian
+  xi <- x
+  hessian[1, 1] <- (n * beta / alpha^2) - (2 * sum((beta^2 / xi^2) * ((xi / alpha)^(beta - 1)) / ((1 + (xi / alpha)^beta)^2)))
+  hessian[2, 2] <- -(n / beta^2) - (2 * sum(- (xi^beta * log(xi / alpha)) / ((1 + (xi / alpha)^beta)^2)))
+  hessian[2, 1] <- -(n / alpha) + (2 * sum((2 * xi^beta * log(xi / alpha)) / ((1 + (xi / alpha)^beta)^2)))
+  hessian[1, 2] <- sum((2 * xi^beta * log(xi / alpha)) / ((1 + (xi / alpha)^beta)^2))
+  return(hessian)
 }
 
 
-NR_fit_LogLogistic_hessian <- function(x, params_0, eps = 0.0001)
+NR_fit_LogLogistic_hessian <- function(x, params_0, eps = 0.00001)
 {
   params <- params_0
   n <- length(x)
@@ -141,28 +39,19 @@ NR_fit_LogLogistic_hessian <- function(x, params_0, eps = 0.0001)
   alpha <- params_0[1]
   beta <- params_0[2]
   
-  while (any(diff > eps))
+  while (diff > eps)
   {
     params.old <- params
-    
     # Gradient vector
-    logx_over_alpha <- log(x/alpha)
-    if (any(is.nan(logx_over_alpha))) {
-      s <- c(0, 0)
-    } else {
-      s <- c(n/beta - sum(log(1 + (x/alpha)^beta)), 
-             -n*alpha/beta^2 +
-               sum(logx_over_alpha * (x/alpha)^beta * (1 + (x/alpha)^beta)^-2))
-    }
+    s <- c(score_alpha(x, alpha, beta), score_beta(x, alpha, beta))
     
-    # Calculate Hessian matrix unless there are NaNs
-    if (!any(is.nan(logx_over_alpha))) {
-      H <- loglogistic_hessian(x, n, alpha, beta)
-      # Update parametres using hessian matrix
-      params <- params - solve(H) %*% s
-      alpha <- params[1]
-      beta <- params[2]
-    }
+    # Calculate Hessian matrix
+    H <- loglogistic_hessian(x, n, alpha, beta)
+    
+    # Update parametres using hessian matrix
+    params <- params - solve(H) %*% s
+    alpha <- params[1]
+    beta <- params[2]
     
     diff <- abs(sum(params - params.old))
   }
@@ -170,5 +59,5 @@ NR_fit_LogLogistic_hessian <- function(x, params_0, eps = 0.0001)
   list(params = params, H = H)
 }
 
-NR_fit_LogLogistic_hessian(gpigs.m43, c(150, 3))
-# IT WOOOOORKS WOOHOOOOO
+# NR_fit_LogLogistic_hessian(gpigs.m43, c(150, 3))
+# pour comparaison le résultat de fitdist:  alpha = 152.389696   beta = 3.012415 
